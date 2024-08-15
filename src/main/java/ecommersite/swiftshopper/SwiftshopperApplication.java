508fcb5e5e84552ae8736935d2a7062369a35cab
Debayan Das <debayandas.185@gmail.com>
2024-08-15 17:07:09 +0530

    Initial commit

---
+++ src/main/java/ecommersite/swiftshopper/SwiftshopperApplication.java
@@ -0,0 +1,13 @@
+package ecommersite.swiftshopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftshopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftshopperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecommersite/swiftshopper/SwiftshopperApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommersite.swiftshopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftshopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
